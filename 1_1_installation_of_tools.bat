5cead9edb5d7f3711cda5d62a00da5629663142b
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2026-01-20 20:55:15 +0530

    feat: 1.1. code

---
+++ 1_1_installation_of_tools.bat
@@ -0,0 +1,11 @@
+winget install --id Microsoft.VisualStudioCode -e
+winget install --id Git.Git -e
+winget install --id Python.Python.3 -e
+code --version
+git --version
+python --version
+code --install-extension zhuangtongfa.Material-theme
+code --install-extension GitHub.github-vscode-theme
+code --install-extension sdras.night-owl
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension GitHub.github-vscode-theme
